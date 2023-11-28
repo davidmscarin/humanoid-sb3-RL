bcae1cd1cd7e49a8bd2f3faf319d36b708ec9a5a
Panda-Hacks <2003pars@gmail.com>
2023-11-28 20:45:28 +0000

    its a start

---
+++ Env_test.py
@@ -0,0 +1,22 @@
+import gymnasium as gym
+
+env = gym.make('Humanoid-v4')
+
+#Let's take a peak at the lunar lander environment:
+
+# Create the environment
+env = gym.make('LunarLander-v2')  # continuous: LunarLanderContinuous-v2
+
+# required before you can step the environment
+env.reset()
+
+# sample action:
+print("sample action:", env.action_space.sample())
+
+# observation space shape:
+print("observation space shape:", env.observation_space.shape)
+
+# sample observation:
+print("sample observation:", env.observation_space.sample())
+
+env.close()
